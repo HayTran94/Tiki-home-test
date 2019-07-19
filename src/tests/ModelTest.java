ab3e137ce8d37485308ae83556220f85a1845101
Hay Tran <haytran@fiot.vn>
2019-07-20 00:27:37 +0700

    update test

--- src/tests/ModelTest.java
+++ src/tests/ModelTest.java
@@ -8,31 +8,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ModelTest extends TestCase {
-    private User user;
     private Product appleProduct;
     private Product orangeProduct;
     @Before
     public void setUp() {
-        user = new User("John Doe", "john.doe@example.com");
         appleProduct = new Product("Apple", 4.95);
         orangeProduct = new Product("Orange", 3.99);
     }
 
     @Test
     public void test1() {
+        User user = new User("John Doe", "john.doe@example.com");
         ShoppingCart cart = user.createShopingCart();
         cart.addProduct(appleProduct, 1);
         cart.addProduct(appleProduct, 1);
         cart.addProduct(orangeProduct, 1);
-        Assert.assertEquals(cart.calculateTotalPrice(), 4.95 * 2 + 3.99, 0.001f);
+        Assert.assertEquals(cart.calculateTotalPrice(), 4.95 * 2 + 3.99, 0.01f);
     }
 
     @Test
     public void test2() {
+        User user = new User("John Doe", "john.doe@example.com");
         ShoppingCart cart = user.createShopingCart();
         cart.addProduct(appleProduct, 3);
         cart.removeProduct(appleProduct, 1);
-        Assert.assertEquals(cart.calculateTotalPrice(), 4.95 * 2, 0.001f);
+        Assert.assertEquals(cart.calculateTotalPrice(), 4.95 * 2, 0.01f);
     }
 
 }
